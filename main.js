de23c4938e0d0920cca84fbc6a63e5c141922137
Nayf Alassaf <Nayf-Alassaf@users.noreply.github.com>
2023-10-31 00:23:40 +0200

    Movies-website

---
+++ main.js
@@ -0,0 +1,54 @@
+// header shadow
+let header = document.querySelector('header');
+window.addEventListener('scroll',() => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// menu bar
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+// swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+});
+
+// swiper
+var swiper = new Swiper(".coming-container", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+    delay: 55000,
+    disableOnInteraction: false,
+    },
+    centeredSlides:true,
+    breakpoints:{
+        0:{
+            slidesPerView: 2,
+        },
+        568:{
+            slidesPerView: 3,
+        },
+        768:{
+            slidesPerView: 4,
+        },
+        968:{
+            slidesPerView: 5,
+        },
+    },
+});
